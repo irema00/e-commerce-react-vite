db5962aca9b629ff6f55a8b984f6eed924cdf89b
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-09 14:42:47 +0300

    deletes unnecessary carousel import

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from "../layouts/Header"
-import { Carousel } from "@material-tailwind/react"
 import { CarouselCustomNavigation } from "../utils/CarouselComponent"
 
 export default function Home() {
