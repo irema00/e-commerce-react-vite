08edd6146d7df32f7a86e37847ca6da98b4c36b0
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-10 19:52:22 +0300

    formats document

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Home from "./pages/Home"
+import "./App.css";
 
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-    <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
