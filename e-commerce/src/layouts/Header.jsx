ed5641b8ce3b0675688fa5d789e0c71038abcff4
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-10 19:48:30 +0300

    completes dark navbar without links

--- e-commerce/src/layouts/Header.jsx
+++ e-commerce/src/layouts/Header.jsx
@@ -3,25 +3,55 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
   return (
-    <>
-    <div className="bg-darkBg text-whiteText flex flex-row gap-2 font-montserrat leading-normal tracking-tight" >
-      <div><FontAwesomeIcon icon={faPhone} style={{color: "#ffffff",}} /> (225) 555-0118</div>
-    <div><FontAwesomeIcon icon={faEnvelope} /> michelle.rivera@example.com</div>
-      <div>Follow Us and get a chance to win 80% off</div>
-      <div className="flex flex-"><div className="text-white text-sm font-bold font-monserrat leading-normal tracking-tight">Follow Us :</div>icons</div>
-    </div>
-    <div>
-      <div> BrandName </div>
-      <div><div>Home</div>
-      <div>Shop</div>
-      <div>About</div>
-      <div>Blog</div>
-      <div>Contact</div>
-      <div>Pages</div></div>
-<div> <div>icon Login/Register</div></div>
-<div><div>incele</div>
-<div>cart</div>
-<div>heart</div></div>
+      <div className="bg-darkBg text-whiteText flex justify-between items-center flex-wrap gap-2 font-montserrat font-semibold text-sm px-10 h-[54px]">
+        <div className="flex items-center gap-7 font-semibold ">
+          <div className="flex items-center gap-1">
+            <FontAwesomeIcon
+              icon={faPhone}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+            <p>(225) 555-0118 </p>
+          </div>
+          <div className="flex items-center gap-1">
+            <FontAwesomeIcon size="lg" icon={faEnvelope} />
+            <p>michelle.rivera@example.com</p>
+          </div>
+        </div>
+
+        <div className="font-semibold text-sm ">
+          Follow Us and get a chance to win 80% off
+        </div>
+
+        <div className="flex gap-4 justify-center items-center">
+          <div className="text-white text-sm font-bold font-monserrat leading-normal tracking-tight flex flex-row gap-2 ">
+            <p>Follow Us</p> <span>:</span>
+          </div>
+          <div className="flex gap-4">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+            <FontAwesomeIcon
+              icon={faYoutube}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+            <FontAwesomeIcon
+              icon={faFacebook}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </div>
+        </div>
+      </div>
+
     </div>
     
     </>
