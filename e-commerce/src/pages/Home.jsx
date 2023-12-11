63978ff9cd36ea40d4c26e57e0ed5900860570c7
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-11 06:42:28 +0300

    renders carousel bottom

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import { CarouselTop } from "../utils/CarouselTop";
+import { CarouselBottom } from "../utils/CarouselBottom";
 
 export default function Home() {
   return (
     <>
       <Header />
       <CarouselTop />
+      <CarouselBottom />
       <Footer />
     </>
   );
