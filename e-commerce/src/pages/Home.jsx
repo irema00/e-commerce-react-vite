6033d48013df41fdefc5fc175c02fd070f9ac7d8
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-11 05:58:34 +0300

    renders footer in home page

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../layouts/Header";
-import { CarouselTop } from "../utils/CarouselComponent";
+import Footer from "../layouts/Footer";
+import { CarouselTop } from "../utils/CarouselTop";
 
 export default function Home() {
   return (
     <>
       <Header />
       <CarouselTop />
+      <Footer />
     </>
   );
 }
