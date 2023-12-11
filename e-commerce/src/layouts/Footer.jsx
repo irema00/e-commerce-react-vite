e864bdc5d83a79a6815a41a982d8cea4ffca23bc
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-11 05:58:02 +0300

    completes footer

---
+++ e-commerce/src/layouts/Footer.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+  const footerSections = [
+    {
+      title: "Company Info",
+      links: ["About Us", "Carrier", "We are hiring", "Blog"],
+    },
+    {
+      title: "Legal",
+      links: ["About Us", "Carrier", "We are hiring", "Blog"],
+    },
+    {
+      title: "Features",
+      links: [
+        "Business Marketing",
+        "User Analytic",
+        "Live Chat",
+        "Unlimited Support",
+      ],
+    },
+    {
+      title: "Resources",
+      links: ["IOS & Android", "Watch a Demo", "Customers", "API"],
+    },
+  ];
+
+  return (
+    <div className=" text-darkBg  font-montserrat ">
+      <div className=" bg-ltGrey  px-[13%] items-center">
+        <div className="flex justify-between py-6 ">
+          <p className="text-2xl font-bold">Bandage</p>
+
+          <div className="flex items-center space-x-4 ">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="text-prBlue h-6 w-6"
+            />
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="text-prBlue h-6 w-6"
+            />
+            <FontAwesomeIcon icon={faTwitter} className="text-prBlue h-6 w-6" />
+          </div>
+        </div>
+      </div>{" "}
+      <hr class="border-1 border-solid border-inputBorder mx-[13%] "></hr>
+      <div className="bg-white  px-[13%]   py-8  font-montserrat font-bold   ">
+        <div className=" bg-white flex flex-wrap justify-between  items-start  ">
+          {footerSections.map((section, index) => (
+            <div key={index} className=" mb-4 md:mb-0 md:w-1/5">
+              <h5 className="text-lg font-semibold">{section.title}</h5>
+              <ul className="mt-4">
+                {section.links.map((link, linkIndex) => (
+                  <li
+                    key={linkIndex}
+                    className="mt-2 text-sm text-hdGrey leading-6 tracking-wide"
+                  >
+                    {link}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className="w-full md:w-1/5 mt-8 md:mt-0 flex flex-col">
+            <h5 className="text-lg font-semibold mb-4">Get In Touch</h5>
+            <form className="flex">
+              <input
+                type="email"
+                placeholder="Your Email"
+                className=" py-3 px-1 border
+              border-solid border-inputBorder rounded-l-md focus:outline-none focus:border-prBlue bg-ltGrey  text-hdGrey  font-normal "
+              />
+              <button
+                type="submit"
+                className="bg-prBlue px-3 text-white text-sm font-light rounded-r-md hover:bg-blue-600"
+              >
+                Subscribe
+              </button>
+            </form>
+            <p className="mt-4 text-sm leading-7 text-hdGrey">
+              Lore imp sum dolor Amit
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="text-left font-montserrat font-bold px-[13%] text-hdGrey flex items-center bg-ltGrey py-4">
+        Made With Love By Finland All Right Reserved
+      </p>
+    </div>
+  );
+}
