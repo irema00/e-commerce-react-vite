bb8b4b0d7aec0b4b9f011cbb9f7020cc316ef36e
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-12 00:19:45 +0300

    adds footer data

--- e-commerce/src/data.jsx
+++ e-commerce/src/data.jsx
@@ -166,29 +166,36 @@ export const editorsPick = {
 };
 /*FOOTER*/
 
-export const footerSections = [
-  {
-    title: "Company Info",
-    links: ["About Us", "Carrier", "We are hiring", "Blog"],
-  },
-  {
-    title: "Legal",
-    links: ["About Us", "Carrier", "We are hiring", "Blog"],
-  },
-  {
-    title: "Features",
-    links: [
-      "Business Marketing",
-      "User Analytic",
-      "Live Chat",
-      "Unlimited Support",
-    ],
-  },
-  {
-    title: "Resources",
-    links: ["IOS & Android", "Watch a Demo", "Customers", "API"],
-  },
-];
+export const footerData = {
+  brandName: "Bandage",
+  copyright: "  Made With Love By Finland All Right Reserved",
+  buttonText: "Subscribe",
+  inputTitle: "Get In Touch",
+  inputSubtext: " Lore imp sum dolor Amit",
+  footerSections: [
+    {
+      title: "Company Info",
+      links: ["About Us", "Carrier", "We are hiring", "Blog"],
+    },
+    {
+      title: "Legal",
+      links: ["About Us", "Carrier", "We are hiring", "Blog"],
+    },
+    {
+      title: "Features",
+      links: [
+        "Business Marketing",
+        "User Analytic",
+        "Live Chat",
+        "Unlimited Support",
+      ],
+    },
+    {
+      title: "Resources",
+      links: ["IOS & Android", "Watch a Demo", "Customers", "API"],
+    },
+  ],
+};
 
 export const data = {
   carousel1,
@@ -196,5 +203,5 @@ export const data = {
   partOfTheNeuralUniverse,
   bestsellerProducts,
   editorsPick,
-  footerSections,
+  footerData,
 };
