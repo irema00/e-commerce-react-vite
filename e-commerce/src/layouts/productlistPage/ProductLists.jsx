5c978ce3a676e445a166fe96baaf4985f9b33a06
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-13 12:30:55 +0300

    edits product list by using product card

--- e-commerce/src/layouts/productlistPage/ProductLists.jsx
+++ e-commerce/src/layouts/productlistPage/ProductLists.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useData } from "../../contexts/DataContext";
 import { Icon } from "@iconify/react";
+import ProductCard from "../../components/ProductCard";
 
 export default function ProductLists() {
   const { productListData } = useData();
@@ -43,38 +44,7 @@ export default function ProductLists() {
       </div>
       <div className="flex flex-wrap justify-evenly gap-3">
         {productListData.products.map((product, index) => (
-          <div
-            key={index}
-            className="flex flex-col flex-wrap items-center mb-4 shadow-xl  hover:scale-110 cursor-pointer  "
-          >
-            <img
-              src={product.imageUrl}
-              alt={product.title}
-              className="object-cover object-center max-w-[240px] max-h-[450px] cursor-pointer  " /* resme sabit genişlik ve yükseklik, değiştirilebilir*/
-            />
-            <div className="flex gap-2 flex-col items-center mt-3 mb-5 ">
-              <h3 className="w-32 text-center text-darkBg text-base font-bold  leading-normal tracking-tight  mt-2 ">
-                {product.title}
-              </h3>
-              <p className="text-center text-hdGrey text-sm font-bold leading-normal tracking-tight ">
-                {product.department}
-              </p>
-              <div className="flex justify-evenly w-full">
-                <span className=" text-semiGrey text-base font-bold  leading-normal tracking-tight ">
-                  {product.price}
-                </span>
-                <span className="text-teal-700 text-base font-bold  leading-normal tracking-tight ">
-                  {product.newPrice}
-                </span>
-              </div>
-              <div className="flex items-center gap-1 ">
-                <div className="w-4 h-4 bg-blue-500 rounded-full" />
-                <div className="w-4 h-4 bg-teal-700 rounded-full" />
-                <div className="w-4 h-4 bg-orange-400 rounded-full" />
-                <div className="w-4 h-4 bg-black rounded-full" />
-              </div>
-            </div>
-          </div>
+          <ProductCard key={index} product={product} />
         ))}
       </div>
     </div>
