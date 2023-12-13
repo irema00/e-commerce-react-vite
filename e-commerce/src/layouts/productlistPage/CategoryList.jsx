6d9e12ae7038db6a5d99d6097030fd6a474e1968
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-13 14:27:52 +0300

    creates category list be shown  on productListPage

---
+++ e-commerce/src/layouts/productlistPage/CategoryList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CategoryCard from "../../components/CategoryCard";
+import { useData } from "../../contexts/DataContext";
+
+const CategoryList = () => {
+  const { categoryData } = useData();
+
+  return (
+    <div className="flex justify-center items-center gap-5 ">
+      {categoryData.map((category) => (
+        <CategoryCard
+          key={category.id}
+          category={category.categoryName}
+          itemCount={category.itemCount}
+          imageUrl={category.imageUrl}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
