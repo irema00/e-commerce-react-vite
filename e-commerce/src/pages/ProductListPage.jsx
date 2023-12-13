1b1f15212d39c08bcdf692e6154d4e961d5a338e
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-13 14:28:02 +0300

    renders categoryList

--- e-commerce/src/pages/ProductListPage.jsx
+++ e-commerce/src/pages/ProductListPage.jsx
@@ -3,11 +3,13 @@ import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import Clients from "../layouts/productlistPage/Clients";
 import ProductLists from "../layouts/productlistPage/productLists";
+import CategoryList from "../layouts/productlistPage/CategoryList";
 
 export default function ProductListPage() {
   return (
     <>
       <Header />
+      <CategoryList />
       <ProductLists />
       <Clients />
       <Footer />
