084f9a11ed9a81adadfcdac78a4b6564801b7e27
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-14 05:30:00 +0300

    completes product detail bestseller part with extra circles

---
+++ e-commerce/src/layouts/productPage/ProductDetailBestseller.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useData } from "../../contexts/DataContext";
+import ProductCard from "../../components/ProductCard";
+
+export default function ProductDetailBestseller() {
+  const { productDetailBestsellerData } = useData();
+  return (
+    <div className="flex flex-col px-[10%]  py-5">
+      <div className="flex pt-10 py-6 items-center font-bold text-3xl tracking-tight">
+        {productDetailBestsellerData.title}
+      </div>
+      <div className="flex flex-wrap justify-between py-3 pt-6 mb-5 border-t-2 border-solid  cursor-pointer border-gray-300 ">
+        {productDetailBestsellerData.products.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
