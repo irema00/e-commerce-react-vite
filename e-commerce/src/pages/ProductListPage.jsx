06f8595bf713cf1f59a3c792966c92e3b1526be1
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-14 05:30:49 +0300

    corrects typo

--- e-commerce/src/pages/ProductListPage.jsx
+++ e-commerce/src/pages/ProductListPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import Clients from "../layouts/productlistPage/Clients";
-import ProductLists from "../layouts/productlistPage/productLists";
+import ProductLists from "../layouts/productlistPage/ProductLists";
 import CategoryList from "../layouts/productlistPage/CategoryList";
 import PageBanner from "../layouts/productlistPage/PageBanner";
 
