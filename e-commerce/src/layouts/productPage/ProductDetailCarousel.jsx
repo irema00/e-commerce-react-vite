a12d69ecf7241cebb83604bc8f6ced2f0154d882
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-14 04:13:51 +0300

    adjusts the w-h of carousel and thumbnail images

--- e-commerce/src/layouts/productPage/ProductDetailCarousel.jsx
+++ e-commerce/src/layouts/productPage/ProductDetailCarousel.jsx
@@ -7,8 +7,8 @@ export default function ProductDetailCarousel() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className=" flex flex-col w-[300px] h-[500px]">
-      <Carousel className="w-[300px] h-[267px]">
+    <div className=" flex flex-col w-[500px] h-[525px] ">
+      <Carousel className="w-[450px] h-[500px]">
         {productData.imageUrl.map((img, idx) => (
           <div key={idx} className="flex">
             <img
@@ -19,13 +19,14 @@ export default function ProductDetailCarousel() {
           </div>
         ))}
       </Carousel>
-      <div className="flex items-center justify-start flex-wrap">
+      <div className="flex items-center justify-start flex-wrap gap-5">
         {productData.optionsUrl.map((opt, idx) => (
           <div
             key={idx}
-            className={`w-20 h-20 p-2 ${
-              activeIndex === idx ? "border-4 border-solid border-blue-500" : ""
-            }`}
+            className="w-[100px] h-[100px]  "
+            // className={`w-20 h-20 p-2 ${
+            //   activeIndex === idx ? "border-4 border-solid border-blue-500" : ""
+            // }`}
           >
             <img
               src={opt}
