9bdc1c3a252b4e787cada6f7047d7b25aa67d5ac
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-15 09:33:59 +0300

    creates team page template

--- e-commerce/src/layouts/PageContent.jsx
+++ e-commerce/src/layouts/PageContent.jsx
@@ -4,6 +4,7 @@ import ProductListPage from "../pages/ProductListPage";
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
 import PageNotFound from "../pages/PageNotFound";
+import TeamPage from "../pages/TeamPage";
 
 export default function PageContent() {
   return (
@@ -12,6 +13,7 @@ export default function PageContent() {
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ProductListPage />} />
         <Route path="/product" element={<ProductPage />} />
+        <Route path="/team" element={<TeamPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
