caaf14d8801d12f6c7128b8f2303e18b67be4043
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-16 15:09:30 +0300

    adds components to render contact page

--- e-commerce/src/pages/ContactPage.jsx
+++ e-commerce/src/pages/ContactPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import TeamPage from "../layouts/contactPage/TeamPage";
+import ContactInfo from "../layouts/contactPage/ContactInfo";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
 
 export default function ContactPage() {
   return (
     <div>
+      <Header />
       <TeamPage />
+      <ContactInfo />
+      <Footer />
     </div>
   );
 }
