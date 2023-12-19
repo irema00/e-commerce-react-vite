30ead4e9cb8c6c2cd4e38d00b47b6308e563ef83
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-19 23:59:28 +0300

    completes bestseller responsive

--- e-commerce/src/layouts/homepage/Bestseller.jsx
+++ e-commerce/src/layouts/homepage/Bestseller.jsx
@@ -6,15 +6,15 @@ export default function Bestseller() {
   const { bestsellerData } = useData();
 
   return (
-    <div className="flex flex-col flex-wrap py-20 lg:px-[10%] px-0 font-monserrat gap-12 ">
+    <div className="flex flex-col flex-wrap py-[5%] lg:px-[12%] px-0 font-monserrat gap-12 ">
       <div className="flex text-center flex-col py-8 ">
         <div className="text-center text-hdGrey text-xl font-normal leading-loose tracking-tight">
           {bestsellerData.topTitle}
         </div>
-        <h2 className="font-bold text-2xl tracking-normal mb-2">
+        <h2 className="font-bold text-xl tracking-normal mb-2">
           {bestsellerData.title}
         </h2>
-        <p className="font-normal text-md text-gray-600">
+        <p className="md:text-md font-bold text-xs sm:px-0 px-[20%] tracking-wide text-gray-500">
           {bestsellerData.description}
         </p>
       </div>
