04524e415710e6e3a33b51d5500dcc32b019c64d
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-20 22:19:32 +0300

    responsive team page

--- e-commerce/src/layouts/contactPage/TeamPage.jsx
+++ e-commerce/src/layouts/contactPage/TeamPage.jsx
@@ -5,7 +5,7 @@ export default function TeamPage() {
   const { featuredPostsData, teamData } = useData();
 
   return (
-    <div className="font-montserrat px-[15%]  py-[5%] flex flex-col items-center justify-center cursor-pointer">
+    <div className="font-montserrat px-[10%]  py-[5%] flex flex-col items-center justify-center cursor-pointer">
       <div className="flex flex-col text-center  mb-5">
         <h1 className=" text-darkBg text-3xl  tracking-normal font-bold mb-4">
           Meet Our Team
@@ -18,13 +18,13 @@ export default function TeamPage() {
         </p>
       </div>
 
-      <div className="flex flex-wrap  md:justify-between justify-center py-3 pt-6  gap-4  cursor-pointer  ">
+      <div className="flex flex-wrap  md:justify-between justify-center py-3 pt-6  gap-8 cursor-pointer  ">
         {teamData.members.map((member, index) => (
           <div key={index} className="team-member-card">
             <img
               src={member.imageURL}
               alt={member.name}
-              className="w-[220px] rounded-xl mb-3 "
+              className="xl:w-[260px] lg:w-[220px] sm:w-[250px] w-[300px] rounded-xl mb-3 "
             />
             <h3 className="font-bold text-darkBg mb-2 ">{member.name}</h3>
             <p className="font-semibold text-hdGrey text-xs">{member.job}</p>
