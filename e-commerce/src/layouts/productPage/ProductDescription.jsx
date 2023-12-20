a40b10b212c0450d00432760df511e42e897a402
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-20 04:03:48 +0300

    product description responsive

--- e-commerce/src/layouts/productPage/ProductDescription.jsx
+++ e-commerce/src/layouts/productPage/ProductDescription.jsx
@@ -6,8 +6,8 @@ export default function ProductDescription() {
   const { productData } = useData();
 
   return (
-    <div className="flex flex-col md:px-[10%] px-[5%]">
-      <div className="flex flex-row md:justify-center justify-evenly items-center py-[3%] font-extrabold md:text-lg text-xs text-hdGrey md:gap-8 md:border-b border-solid border-semiGrey border-opacity-">
+    <div className="flex flex-col md:px-[10%] px-[5%] ">
+      <div className="flex flex-row  md:justify-center justify-evenly items-center py-[3%] font-extrabold md:text-lg text-xs text-hdGrey md:gap-8 md:border-b border-solid border-semiGrey border-opacity-">
         <p className="font-bold md:no-underline text-underline ">Description</p>
         <p>Additional Information</p>
         <p>
@@ -15,9 +15,9 @@ export default function ProductDescription() {
         </p>
       </div>
 
-      <div className="flex items-start justify-between flex-wrap  mt-6 ">
-        <div className="py-[3%] flex justify-center border-solid border-t-semiGrey md:basis-1/3">
-          <div className=" relative bg-detailImgBg rounded-lg flex md:w-[300px] md:h-[350px] w-[300px] h-[275px]">
+      <div className="flex justify-between gap-10 flex-wrap lg:flex-nowrap ">
+        <div className="py-[3%] flex justify-center border-solid border-t-semiGrey  w-full">
+          <div className=" relative bg-detailImgBg rounded-lg flex md:w-[300px] md:h-[350px] w-[300px] h-[275px] px-[5%] lg:px-0">
             <img
               className="flex absolute rounded-lg  h-full w-full -left-3 -top-3  justify-center "
               src={productData.detailImgUrl}
@@ -26,25 +26,25 @@ export default function ProductDescription() {
           </div>
         </div>
 
-        <div className="flex flex-col md:basis-1/3 gap-5 ">
-          <div className="flex flex-col md:basis-1/3 text-slate-800 text-2xl font-bold  leading-loose tracking-tight">
+        <div className="flex flex-col gap-5 px-[5%] lg:px-0 w-full">
+          <div className="flex flex-col  text-slate-800 text-2xl font-bold  leading-loose tracking-tight">
             <h2>{productData.detailText}</h2>
           </div>
-          <div className="flex text-slate-800 text-2xl font-bold  leading-loose tracking-tight">
+          <div className="flex text-slate-800 text-lg font-bold  leading-loose tracking-tight text-hdGrey">
             <p className="flex">{productData.detailPg}</p>
           </div>
-          <div className="flex text-slate-800 text-2xl font-bold  leading-loose tracking-tight">
+          <div className="flex text-slate-800 text-lg font-bold  leading-loose tracking-tight text-hdGrey">
             <div>
               <p className="flex">{productData.detailPg}</p>{" "}
             </div>
           </div>
-          <div className="flex text-slate-800 text-2xl font-bold  leading-loose tracking-tight">
+          <div className="flex text-slate-800 text-lg font-bold  leading-loose tracking-tight text-hdGrey">
             <p className="flex">{productData.detailPg}</p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-7 ">
-          <div className="flex flex-col md:basis-1/3 text-slate-800 text-2xl font-bold  leading-loose tracking-wide gap-4 ">
+        <div className="flex flex-col gap-7  px-[5%] lg:px-0 w-full">
+          <div className="flex flex-col  text-slate-800 text-2xl font-bold  leading-loose tracking-wide gap-4  ">
             <h2>{productData.detailTitle}</h2>
             <div className="flex gap-3">
               <Icon icon="teenyicons:right-outline" color="gray" />
@@ -72,7 +72,7 @@ export default function ProductDescription() {
             </div>
           </div>
 
-          <div className="flex flex-col md:basis-1/3 text-slate-800 text-2xl font-bold  leading-loose tracking-tight gap-4 ">
+          <div className="flex flex-col text-slate-800 text-2xl font-bold  leading-loose tracking-tight gap-4 ">
             <h2>{productData.detailTitle}</h2>
             <div className="flex gap-3">
               <Icon icon="teenyicons:right-outline" color="gray" />
