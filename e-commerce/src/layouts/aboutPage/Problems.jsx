4a87cb24e3f8de7e8d8bacf6c867182c4d4f62f4
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-20 22:19:15 +0300

    responsive problem

--- e-commerce/src/layouts/aboutPage/Problems.jsx
+++ e-commerce/src/layouts/aboutPage/Problems.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function Problems() {
   return (
-    <div className="flex items-start flex-wrap px-[10%] py-[2%] flex-col  ">
+    <div className="flex flex-wrap   px-[10%] flex-col  py-[15%] lg:py-[5%] text-center items-center lg:items-start lg:justify-start">
       <div className="text-red-500 text-lg font-semibold mb-5  ">
         Problems trying
       </div>
-      <div className="flex items-start flex-wrap lg:gap-0 gap-8 ">
-        <div className=" text-slate-800 text-4xl font-bold leading-normal tracking-tight w-1/2 ">
+      <div className="flex items-start flex-wrap lg:flex-nowrap gap-8 lg:text-start text-center w-full  ">
+        <div
+          className=" text-slate-800 text-4xl font-bold leading-normal tracking-tight  lg:w-1/2
+        "
+        >
           Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
         </div>
-        <div className="text-hdGrey text-md font-semibold leading-normal  tracking-tight w-1/2 ">
+        <div
+          className="text-hdGrey text-md font-semibold leading-normal  tracking-tight lg:w-1/2
+        "
+        >
           Problems trying to resolve the conflict between the two major realms
           of Classical physics: Newtonian mechanics
         </div>
