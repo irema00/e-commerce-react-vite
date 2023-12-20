cf8045f54fc4e89be7ff945cf12086e6ac463938
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-21 01:31:56 +0300

    imports toast container

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { DataProvider } from "./contexts/DataContext";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import PageContent from "./layouts/PageContent";
 
 function App() {
   return (
     <DataProvider>
       <PageContent />
+      <ToastContainer />
     </DataProvider>
   );
 }
