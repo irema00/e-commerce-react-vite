e1dbb02eb491aed947a3591965004bd03fc173e5
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-20 04:03:59 +0300

    clients responsive

--- e-commerce/src/layouts/productlistPage/Clients.jsx
+++ e-commerce/src/layouts/productlistPage/Clients.jsx
@@ -6,7 +6,7 @@ export default function Clients() {
 
   return (
     <div className="flex items-center px-[10%] py-[3%] w-full flex-wrap bg-ltGrey ">
-      <div className="flex justify-between lg:flex-row flex-col items-center lg:py-0 py-[15%] lg:no-gap gap-12 flex-wrap w-full">
+      <div className="flex justify-between  items-center lg:py-0 py-[15%] lg:gap-8 gap-12 flex-wrap w-full sm:flex-row sm:px-[10%] sm:gap-20 lg:px-0 flex-col">
         {clientsData.logos.map((logo, index) => (
           <img
             className="cursor-pointer hover:animate-spin"
