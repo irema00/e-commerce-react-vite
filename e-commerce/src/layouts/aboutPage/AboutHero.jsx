3d3adc80414b3926839650c5f84b37e23a298670
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-20 22:18:59 +0300

    responsive about hero

--- e-commerce/src/layouts/aboutPage/AboutHero.jsx
+++ e-commerce/src/layouts/aboutPage/AboutHero.jsx
@@ -9,11 +9,11 @@ import technology1 from "../../assets/contact/technology1.png";
 
 export default function AboutHero() {
   return (
-    <div className="flex w-screen h-full justify-between px-[10%] py-[5%]">
-      <div className="flex flex-col justify-center gap-8 items-start ">
+    <div className="flex flex-wrap lg:flex-nowrap justify-between w-full h-full lg:px-[10%] md:px-[25%] py-[5%]">
+      <div className="flex flex-col justify-center gap-8 lg:items-start items-center lg:text-start text-center py-[10%] lg:py-0 w-full">
         <h5 className="text-sm font-bold tracking-tight">ABOUT COMPANY</h5>
         <h1 className="text-5xl font-bold">ABOUT US</h1>
-        <p className="text-lg font-semibold text-hdGrey w-[350px]">
+        <p className="md:text-lg text-md font-semibold text-hdGrey lg:w-[350px] w-[300px]">
           We know how large objects will act, but things on a small scale
         </p>
         <button className="bg-prBlue text-white  text-sm font-semibold px-8 py-4 rounded">
@@ -21,7 +21,7 @@ export default function AboutHero() {
         </button>
       </div>
 
-      <div className="flex pl-10  relative  ">
+      <div className="flex pl-20  relative py-[10%] lg:py-0  items-center justify-center overflow-hidden md:overflow-visible">
         <img
           src={elips3}
           className="flex self-start items-start top-0 bottom-0"
@@ -33,7 +33,7 @@ export default function AboutHero() {
         />
         <img
           src={technology1}
-          className="absolute w-auto h-full inset-x-[20%] "
+          className="absolute w-auto h-[120%] lg:left-[20%] left-0 top-0 "
         />
         <img src={elips2} className="flex self-center items-start pl-7" />
         <img
