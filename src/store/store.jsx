613143611af278a9659af0b3e3638cf43e96f8de
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-21 22:02:46 +0300

    imports shoppingCartReducer

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -8,11 +8,13 @@ import logger from "redux-logger";
 import { globalReducer } from "./reducers/globalReducer";
 import { userReducer } from "./reducers/userReducer";
 import { productReducer } from "./reducers/productReducer";
+import { shoppingCartReducer } from "./reducers/shoppingCartReducer";
 
 export const reducers = combineReducers({
   global: globalReducer,
   user: userReducer,
   product: productReducer,
+  shoppingCart: shoppingCartReducer,
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk, logger));
