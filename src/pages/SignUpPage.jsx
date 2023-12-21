63b9258d1a3c4be53e8b12a176747bf5d724eb9b
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-21 14:54:30 +0300

    creates SignUp page and moves SignUp form in it-with header and footer

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpForm from "../components/SignUpForm";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <Header />
+      <SignUpForm />
+      <Footer />
+    </>
+  );
+}
