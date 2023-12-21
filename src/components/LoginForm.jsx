d8a453288403246e0128a26cfb1749bc920d736b
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-22 00:39:26 +0300

    styles login form with tailwind

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -28,44 +28,73 @@ const LoginForm = () => {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   return (
-    <div className="login-form-container">
-      <form onSubmit={handleSubmit(onSubmit)} className="login-form">
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: emailPattern,
-                message: "Invalid email format",
-              },
-            })}
-            className="form-control"
-          />{" "}
-          {errors.email && (
-            <p className="error-message">{errors.email.message}</p>
-          )}
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            {...register("password", { required: "Password is required" })}
-            className="form-control"
-          />{" "}
-          {errors.password && (
-            <p className="error-message">{errors.password.message}</p>
-          )}
-        </div>
-
-        <button type="submit" className="submit-button">
+    <div
+      className="min-h-screen flex flex-col justify-start
+    pt-10 px-6"
+    >
+      <div className="max-w-md w-full mx-auto bg-white p-8">
+        <h2 className="mb-6 text-3xl font-bold text-center text-gray-900">
           Log In
-        </button>
-      </form>
+        </h2>
+        <div className="bg-white p-8 border border-gray-300 mt-6 rounded-lg shadow-lg">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: emailPattern,
+                    message: "Invalid email format",
+                  },
+                })}
+                placeholder="Your Email"
+                className=" w-full p-3 border border-solid bg-ltGrey border-ltGrey rounded-lg text-sm"
+              />{" "}
+              {errors.email && (
+                <p className="mt-1 text-xs text-red-500">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...register("password", { required: "Password is required" })}
+                placeholder="Your Email"
+                className=" w-full p-3 border border-solid bg-ltGrey border-ltGrey rounded-lg text-sm"
+              />{" "}
+              {errors.password && (
+                <p className="mt-1 text-xs text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="flex w-full px-5 py-3 pr-12 bg-blue-600 text-white rounded-lg font-semibold items-center justify-center hover:bg-blue-700"
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
