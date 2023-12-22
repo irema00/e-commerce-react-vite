58c47620d5f074916d9bf9e3cce7b319751fa21f
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-23 00:34:32 +0300

    corrects user reducer

--- src/store/reducers/userReducer.jsx
+++ src/store/reducers/userReducer.jsx
@@ -1,15 +1,30 @@
 const userInitial = {
   user: null,
+  token: null,
   fetchState: "NOT_FETCHED",
+  isLoggedIn: false,
+  error: null,
 };
 
 export const userReducer = (state = userInitial, action) => {
   switch (action.type) {
-    case "SET_USER_NAME":
-      return { ...state, name: action.payload };
+    case "SET_IS_LOGGED_IN":
+      return { ...state, isLoggedIn: action.payload };
 
-    case "SET_USER_EMAIL":
-      return { ...state, email: action.payload };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+        isLoggedIn: true,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        ...state,
+        error: action.payload,
+        isLoggedIn: false,
+      };
 
     default:
       return state;
