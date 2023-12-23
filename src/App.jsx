e000fc95f2594b7012a15d39765eb47717354093
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-23 23:22:34 +0300

    dispatches and sets isLoggedIn to keep userData on screen when user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { DataProvider } from "./contexts/DataContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageContent from "./layouts/PageContent";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setIsLoggedIn } from "./store/actions/userActions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+    if (isLoggedIn) {
+      dispatch(setIsLoggedIn(true));
+    }
+  }, [dispatch]);
   return (
     <DataProvider>
       <PageContent />
