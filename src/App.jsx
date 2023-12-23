aa0bba4d0c1ed434dfbf41e82c55d4801bfab32e
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-24 02:35:47 +0300

    makes get request to verify endpoint to check whether localStorage has token

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { DataProvider } from "./contexts/DataContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageContent from "./layouts/PageContent";
-import { useEffect } from "react";
+import { setUser } from "./store/actions/userActions";
 import { useDispatch } from "react-redux";
 import { setIsLoggedIn } from "./store/actions/userActions";
+import { AxiosInstance, renewAxiosInstance } from "./api/api";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-    if (isLoggedIn) {
-      dispatch(setIsLoggedIn(true));
+    const token = localStorage.getItem("token");
+    if (token) {
+      AxiosInstance.get("/verify")
+        .then((response) => {
+          const user = response.data;
+          dispatch(setUser(user));
+          renewAxiosInstance();
+          console.log("verified", user);
+        })
+        .catch((error) => {
+          console.error("Token verification failed", error);
+          localStorage.removeItem("token");
+          renewAxiosInstance();
+        });
     }
   }, [dispatch]);
   return (
