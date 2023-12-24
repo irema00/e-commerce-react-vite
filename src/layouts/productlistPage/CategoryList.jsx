dd3424f7614897b083ddf5b922f76c6328868da8
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-24 03:57:19 +0300

    renders category cards by using categories from global state

--- src/layouts/productlistPage/CategoryList.jsx
+++ src/layouts/productlistPage/CategoryList.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import CategoryCard from "../../components/CategoryCard";
-import { useData } from "../../contexts/DataContext";
+import { useSelector } from "react-redux";
 
 const CategoryList = () => {
-  const { categoryData } = useData();
-
+  const categories = useSelector((state) => state.global.categories);
   return (
     <div className="flex lg:justify-between justify-center flex-wrap items-center gap-5 bg-ltGrey px-[10%]">
-      {categoryData.map((category) => (
+      {categories.map((category) => (
         <CategoryCard
           key={category.id}
-          category={category.categoryName}
-          itemCount={category.itemCount}
-          imageUrl={category.imageUrl}
+          category={category.title}
+          imageUrl={category.img}
         />
       ))}
     </div>
