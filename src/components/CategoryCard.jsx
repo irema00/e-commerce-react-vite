4a349f49d64f3f95e6e242f31293a2a730c4d0f4
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-24 03:57:49 +0300

    arranges card width and height for different screens

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,10 +1,10 @@
 const CategoryCard = ({ category, itemCount, imageUrl }) => {
   return (
-    <div className="relative flex flex-wrap shadow-xl w-[350px] max-h-[500px] lg:w-auto lg:max-h-58 mb-8 hover:scale-110 cursor-pointer">
+    <div className="relative flex flex-wrap shadow-xl max-w-[350px] max-h-[340px] lg:w-[200px] lg:h-[200px] mb-8 hover:scale-110 cursor-pointer overflow-hidden ">
       <img
         src={imageUrl}
         alt={category}
-        className="w-full h-full object-cover"
+        className="object-cover object-center overflow"
       />
       <div className="absolute w-full h-full flex items-center justify-center bg-black bg-opacity-40 hover:bg-opacity-0 hover:shadow-2xl hover:scale-110 p-4 cursor-pointer">
         <div className="flex flex-col items-center justify-center">
