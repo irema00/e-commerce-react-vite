2d34e7e8399eaff5cfc4441f252fef6c18a64d53
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-24 04:36:51 +0300

    updates card sizes

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,10 +1,10 @@
 const CategoryCard = ({ category, itemCount, imageUrl }) => {
   return (
-    <div className="relative flex flex-wrap shadow-xl max-w-[350px] max-h-[340px] lg:w-[200px] lg:h-[200px] mb-8 hover:scale-110 cursor-pointer overflow-hidden ">
+    <div className="relative flex flex-wrap justify-between shadow-xl max-w-[350px] max-h-[350px] md:max-h-[350px]  lg:w-[220px]  lg:h-[230px]  mb-8 lg:hover:scale-110 cursor-pointer overflow-hidden ">
       <img
         src={imageUrl}
         alt={category}
-        className="object-cover object-center overflow"
+        className="object-cover object-center"
       />
       <div className="absolute w-full h-full flex items-center justify-center bg-black bg-opacity-40 hover:bg-opacity-0 hover:shadow-2xl hover:scale-110 p-4 cursor-pointer">
         <div className="flex flex-col items-center justify-center">
--- src/layouts/productlistPage/CategoryList.jsx
+++ src/layouts/productlistPage/CategoryList.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 const CategoryList = () => {
   const categories = useSelector((state) => state.global.categories);
   return (
-    <div className="flex lg:justify-between justify-center flex-wrap items-center gap-5 bg-ltGrey px-[10%]">
+    <div className="flex lg:justify-between justify-center flex-wrap items-center gap-2 bg-ltGrey px-[10%]">
       {categories.map((category) => (
         <CategoryCard
           key={category.id}
