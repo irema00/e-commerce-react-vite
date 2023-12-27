50a7255bb9a72e1fa69dbe2b6f3eac9e61bd632b
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-28 00:36:57 +0300

    imports useState

--- src/layouts/productlistPage/ProductLists.jsx
+++ src/layouts/productlistPage/ProductLists.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "@iconify/react";
 import ProductCard from "../../components/ProductCard";
