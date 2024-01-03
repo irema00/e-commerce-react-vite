51c9a095896f9bfb5c224f546b2b488e63eedf48
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-03 11:04:41 +0300

    gets product detail from redux store to show images related to correct product and adds a message to be rendered  if image cannot be found

--- src/layouts/productPage/ProductDetailCarousel.jsx
+++ src/layouts/productPage/ProductDetailCarousel.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Carousel } from "@material-tailwind/react";
+import { useSelector } from "react-redux";
 
-export default function ProductDetailCarousel({ productData }) {
+export default function ProductDetailCarousel() {
+  const productDetail = useSelector((state) => state.products.productDetail);
+
+  if (!productDetail || !productDetail.images) {
+    return <div>Image cannot be found </div>;
+  }
   return (
     <div className=" flex flex-col justify-center md:w-[475px] md:h-[525px] ">
       <Carousel className="md:w-[450px] md:h-[500px] w-[325px] h-[325px] overflow-hidden  ">
-        {productData.images.map((img, idx) => (
+        {productDetail.images.map((img, idx) => (
           <div key={idx} className="flex">
             <img
               src={img.url}
@@ -16,7 +22,7 @@ export default function ProductDetailCarousel({ productData }) {
         ))}
       </Carousel>
       <div className="flex items-center justify-start flex-wrap gap-5 mt-5 ">
-        {productData.images.map((opt, idx) => (
+        {productDetail.images.map((opt, idx) => (
           <div key={idx} className="w-[100px] h-[100px]  ">
             <img
               src={opt.url}
