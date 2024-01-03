a6e859cc5941c868bdb14ae802a3d3d130c459b4
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-03 09:05:31 +0300

    adds product image to the carousel

--- src/layouts/productPage/ProductDetailCarousel.jsx
+++ src/layouts/productPage/ProductDetailCarousel.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Carousel } from "@material-tailwind/react";
-import { useData } from "../../contexts/DataContext";
 
-export default function ProductDetailCarousel() {
-  const { productData } = useData();
-  const [activeIndex, setActiveIndex] = useState(0);
-
+export default function ProductDetailCarousel({ productData }) {
   return (
     <div className=" flex flex-col justify-center md:w-[475px] md:h-[525px] ">
-      <Carousel className="md:w-[450px] md:h-[500px] w-[325px] h-[325px] ">
-        {productData.imageUrl.map((img, idx) => (
+      <Carousel className="md:w-[450px] md:h-[500px] w-[325px] h-[325px] overflow-hidden  ">
+        {productData.images.map((img, idx) => (
           <div key={idx} className="flex">
             <img
-              src={img}
+              src={img.url}
               alt={`Product ${idx}`}
-              className="flex object-cover object-center overflow-hidden"
+              className="flex object-fill object-center overflow-hidden w-full"
             />
           </div>
         ))}
       </Carousel>
-      <div className="flex items-center justify-start flex-wrap gap-5">
-        {productData.optionsUrl.map((opt, idx) => (
-          <div
-            key={idx}
-            className="w-[100px] h-[100px]  "
-            // className={`w-20 h-20 p-2 ${
-            //   activeIndex === idx ? "border-4 border-solid border-blue-500" : ""
-            // }`}
-          >
+      <div className="flex items-center justify-start flex-wrap gap-5 mt-5 ">
+        {productData.images.map((opt, idx) => (
+          <div key={idx} className="w-[100px] h-[100px]  ">
             <img
-              src={opt}
+              src={opt.url}
               alt={`Option ${idx}`}
               className="flex object-fill object-center overflow-hidden"
             />
