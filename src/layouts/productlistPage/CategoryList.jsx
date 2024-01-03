888e2920d1ba53aecfd2e7e3027f82d3b192a7eb
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-03 23:34:32 +0300

    navigates category cards to related page

--- src/layouts/productlistPage/CategoryList.jsx
+++ src/layouts/productlistPage/CategoryList.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import CategoryCard from "../../components/CategoryCard";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CategoryList = () => {
   const categories = useSelector((state) => state.global.categories);
   const topCategories = [...categories]
     .sort((a, b) => b.rating - a.rating)
     .slice(0, 5);
+
   return (
     <div className="flex lg:justify-between justify-center flex-wrap items-center gap-2 bg-ltGrey px-[10%]">
       {topCategories.map((category) => (
-        <CategoryCard
-          key={category.id}
-          category={category.title}
-          imageUrl={category.img}
-        />
+        <Link to={`/shop/${category.id}/${category.gender}/${category.title}`}>
+          <CategoryCard
+            key={category.id}
+            category={category.title}
+            imageUrl={category.img}
+            gender={category.gender === "e" ? "erkek" : "kadın"}
+          />
+        </Link>
       ))}
     </div>
   );
