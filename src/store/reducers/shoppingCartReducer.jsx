8abaefaf4ea92c27d5aea2176ab917b1f684189b
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-04 19:13:29 +0300

    updates  quantity of products when added to cart

--- src/store/reducers/shoppingCartReducer.jsx
+++ src/store/reducers/shoppingCartReducer.jsx
@@ -7,7 +7,25 @@ const cartInitial = {
 export const shoppingCartReducer = (state = cartInitial, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return { ...state, cart: [...state.cart, action.payload] };
+      const existingCartItem = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingCartItem) {
+        return {
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, { ...action.payload, quantity: 1 }],
+        };
+      }
     case "REMOVE_FROM_CART":
       return {
         ...state,
