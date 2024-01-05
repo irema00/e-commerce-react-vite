039480a820b7ae00ec09feefba35155d02b71478
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-05 09:57:27 +0300

    edit typo

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "../layouts/Header";
 import Clients from "../layouts/productlistPage/Clients";
 import Footer from "../layouts/Footer";
-import ProductDetail from "../layouts/productPage/productDetail";
-import ProductDetailBestseller from "../layouts/productPage/ProductDetailBestseller";
+import ProductDetail from "../layouts/productPage/ProductDetail.jsx";
+import ProductDetailBestseller from "../layouts/ProductPage/ProductDetailBestseller";
 import ProductDescription from "../layouts/productPage/ProductDescription";
 
 export default function ProductPage() {
