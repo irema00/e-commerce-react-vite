5c98b7aa363eab3f90cc2d8c029583b0ad53082d
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-13 15:56:01 +0300

    displays no product added message and a button linking to shop page when no product is added

--- src/layouts/shoppingCartPage/OrderSummary.jsx
+++ src/layouts/shoppingCartPage/OrderSummary.jsx
@@ -1,38 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function OrderSummary({ total, shippingCost, orderTotal }) {
   return (
-    <div className="bg-ltGrey rounded-xl border border-solid border-semiGrey p-4 w-full lg:w-[400px] h-[300px] justify-between flex flex-col ">
+    <div className="bg-ltGrey rounded-xl border border-solid border-semiGrey p-4 w-full lg:w-[400px] h-[350px] justify-between flex flex-col ">
       <h3 className="text-2xl font-bold mb-4 text-center">Order Summary</h3>
-      <div className="flex flex-col p-2 justify-center ">
-        <div className="mb-4">
-          <div className="flex justify-between mb-2 gap-2">
-            <span className="font-bold">Total Products:</span>
-            <span className="font-normal">${total.toFixed(2)}</span>
-          </div>
-          <div className="flex justify-between mb-2 gap-2">
-            <span className="font-bold">Shipping Total:</span>
-            <span>$ 29.99 </span>
-          </div>
-          {shippingCost === 0 && (
-            <div className="flex justify-between mb-2 items-center gap-2 ">
-              <span className="font-bold">
-                $300 and above Free Shipping (Covered by Seller):
-              </span>
-              <span className="font-bold text-prBlue">-$29.99</span>
+      {total === 0 ? (
+        <div className="flex text-center justify-center items-center flex-col h-full gap-5">
+          <p className="font-semibold text-hdGrey">No products added! </p>
+          <Link
+            to="/shop"
+            className="mt-4  bg-prBlue text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-300 transition duration-300 ease-in-out place-self-center cursor-pointer"
+          >
+            Shop Now!
+          </Link>
+        </div>
+      ) : (
+        <div className="flex flex-col p-2 justify-center ">
+          <div className="mb-4">
+            <div className="flex justify-between mb-2 gap-2">
+              <span className="font-bold">Total Products:</span>
+              <span className="font-normal">${total.toFixed(2)}</span>
             </div>
-          )}
-        </div>
-        <div className="border-t border-semiGrey border-solid pt-4">
-          <div className="flex justify-between">
-            <span className="font-bold ">Total:</span>
-            <span className="font-bold">${orderTotal.toFixed(2)}</span>
+            <div className="flex justify-between mb-2 gap-2">
+              <span className="font-bold">Shipping Total:</span>
+              <span>$ 29.99 </span>
+            </div>
+            {shippingCost === 0 && (
+              <div className="flex justify-between mb-2 items-center gap-2 ">
+                <span className="font-bold">
+                  $300 and above Free Shipping (Covered by Seller):
+                </span>
+                <span className="font-bold text-prBlue">-$29.99</span>
+              </div>
+            )}
           </div>
+          <div className="border-t border-semiGrey border-solid pt-4">
+            <div className="flex justify-between">
+              <span className="font-bold ">Total:</span>
+              <span className="font-bold">${orderTotal.toFixed(2)}</span>
+            </div>
+          </div>
+          <button
+            className="mt-4 w-full bg-prBlue text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-300 transition duration-300 ease-in-out place-self-end"
+            disabled={total === 0}
+          >
+            Complete Order
+          </button>
         </div>
-        <button className="mt-4 w-full bg-prBlue text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-300 transition duration-300 ease-in-out place-self-end">
-          Complete Order
-        </button>
-      </div>
+      )}
     </div>
   );
 }
--- src/layouts/shoppingCartPage/ShoppingCartLayout.jsx
+++ src/layouts/shoppingCartPage/ShoppingCartLayout.jsx
@@ -8,6 +8,7 @@ import {
 } from "../../store/actions/shoppingCartActions";
 import { Icon } from "@iconify/react";
 import OrderSummary from "./OrderSummary";
+import { Link } from "react-router-dom";
 
 const ShoppingCartLayout = () => {
   const cartItems = useSelector((state) => state.shoppingCart.cart);
@@ -27,7 +28,6 @@ const ShoppingCartLayout = () => {
   const handleCheckboxChange = (productId) => {
     dispatch(toggleItemCheck(productId));
   };
-  console.log("SEPET ÜRÜNLERİ", cartItems);
   return (
     <div className="container mx-auto my-6 p-4 bg-ltGrey rounded-xl flex justify-between gap-10">
       <div className="flex flex-col w-full">
@@ -35,114 +35,138 @@ const ShoppingCartLayout = () => {
           My Cart ({cartItems.reduce((total, item) => total + item.count, 0)}{" "}
           Items)
         </h2>
-        {isShippingFree(total) ? (
-          <div className="text-green-600 font-bold mb-2">Free Shipping!</div>
-        ) : (
-          <div className="font-normal mb-2">
-            Add{" "}
-            <span className="font-bold text-red-600 ">
-              ${parseFloat((300 - total).toFixed(2)).toFixed(2)}
-            </span>{" "}
-            worth of products to get{" "}
-            <span className="text-successGreen font-semibold">
-              Free Shipping!
-            </span>
+        {cartItems.length === 0 ? (
+          <div className="flex text-center justify-center items-center flex-col h-full gap-5">
+            <p className="font-semibold text-hdGrey">No products added! </p>
+            <Link
+              to="/shop"
+              className="mt-4  bg-prBlue text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-300 transition duration-300 ease-in-out place-self-center cursor-pointer"
+            >
+              Shop Now!
+            </Link>
           </div>
-        )}
-        <div className="flex flex-col space-y-4  ">
-          {cartItems.map((item) => {
-            const itemTotal = calculateItemTotal(
-              item.count,
-              item.product.price
-            );
-            return (
-              <div
-                key={item.product.id}
-                className="flex items-center justify-between p-4 border border-solid border-semiGrey rounded-xl "
-              >
-                <div className="flex items-center space-x-4">
-                  <input
-                    type="checkbox"
-                    checked={item.checked}
-                    onChange={() => handleCheckboxChange(item.product.id)}
-                  />
-                  <img
-                    src={item.product.images[0].url}
-                    alt={item.product.name}
-                    className="h-14 w-14 rounded-xl object-cover mr-4 object-top"
-                  />
-                  <p className="font-semibold w-[300px]">{item.product.name}</p>
-                </div>
-                <div className="flex items-center ">
-                  <button
-                    className="bg-prBlue text-white text-md rounded-l px-2 py-1"
-                    onClick={() => dispatch(decreaseItemCount(item.product.id))}
-                  >
-                    -
-                  </button>
-                  <span className="border border-solid border-semiGrey text-xs px-3 py-[7px] text-darkBg">
-                    {item.count}
-                  </span>
-                  <button
-                    className="bg-prBlue text-white text-md rounded-r px-2 py-1"
-                    onClick={() => dispatch(increaseItemCount(item.product.id))}
-                  >
-                    +
-                  </button>
-                </div>
-                <span className="font-semibold">${itemTotal}</span>
-
-                <Icon
-                  className="flex justify-end items-end hover:cursor-pointer "
-                  icon="fluent:delete-24-regular"
-                  width="20"
-                  height="20"
-                  onClick={() => dispatch(removeFromCart(item.product.id))}
-                />
+        ) : (
+          <>
+            {isShippingFree(total) ? (
+              <div className="text-green-600 font-bold mb-2">
+                Free Shipping!
               </div>
-            );
-          })}
-        </div>
-        <div className="flex justify-end mt-4  ">
-          <table className="w-[300px] divide-y divide-gray-200 border border-solid border-semiGrey">
-            <tbody className="bg-white divide-y divide-gray-200">
-              <tr>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
-                  Total Price:
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
-                  ${total.toFixed(2)}
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
-                  Shipping:
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
-                  {isShippingFree(total) ? (
-                    <div className="flex gap-2">
-                      <span className="text-blue-500 line-through">$29.99</span>
-                      <span className="text-successGreen">Free!</span>
+            ) : (
+              <div className="font-normal mb-2">
+                Add{" "}
+                <span className="font-bold text-red-600 ">
+                  ${parseFloat((300 - total).toFixed(2)).toFixed(2)}
+                </span>{" "}
+                worth of products to get{" "}
+                <span className="text-successGreen font-semibold">
+                  Free Shipping!
+                </span>
+              </div>
+            )}
+            <div className="flex flex-col space-y-4  ">
+              {cartItems.map((item) => {
+                const itemTotal = calculateItemTotal(
+                  item.count,
+                  item.product.price
+                );
+                return (
+                  <div
+                    key={item.product.id}
+                    className="flex items-center justify-between p-4 border border-solid border-semiGrey rounded-xl "
+                  >
+                    <div className="flex items-center space-x-4">
+                      <input
+                        type="checkbox"
+                        checked={item.checked}
+                        onChange={() => handleCheckboxChange(item.product.id)}
+                      />
+                      <img
+                        src={item.product.images[0].url}
+                        alt={item.product.name}
+                        className="h-14 w-14 rounded-xl object-cover mr-4 object-top"
+                      />
+                      <p className="font-semibold w-[300px]">
+                        {item.product.name}
+                      </p>
                     </div>
-                  ) : (
-                    "$29.99"
-                  )}
-                </td>
-              </tr>
-              <tr className=" border-t border-solid border-semiGrey ">
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
-                  Order Total:
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
-                  $
-                  {isShippingFree(total)
-                    ? total.toFixed(2)
-                    : orderTotal.toFixed(2)}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                    <div className="flex items-center ">
+                      <button
+                        className="bg-prBlue text-white text-md rounded-l px-2 py-1"
+                        onClick={() =>
+                          dispatch(decreaseItemCount(item.product.id))
+                        }
+                      >
+                        -
+                      </button>
+                      <span className="border border-solid border-semiGrey text-xs px-3 py-[7px] text-darkBg">
+                        {item.count}
+                      </span>
+                      <button
+                        className="bg-prBlue text-white text-md rounded-r px-2 py-1"
+                        onClick={() =>
+                          dispatch(increaseItemCount(item.product.id))
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                    <span className="font-semibold">${itemTotal}</span>
+
+                    <Icon
+                      className="flex justify-end items-end hover:cursor-pointer "
+                      icon="fluent:delete-24-regular"
+                      width="20"
+                      height="20"
+                      onClick={() => dispatch(removeFromCart(item.product.id))}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex justify-end mt-4  ">
+              <table className="w-[300px] divide-y divide-gray-200 border border-solid border-semiGrey">
+                <tbody className="bg-white divide-y divide-gray-200">
+                  <tr>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
+                      Total Price:
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
+                      ${total.toFixed(2)}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
+                      Shipping:
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
+                      {isShippingFree(total) ? (
+                        <div className="flex gap-2">
+                          <span className="text-blue-500 line-through">
+                            $29.99
+                          </span>
+                          <span className="text-successGreen">Free!</span>
+                        </div>
+                      ) : (
+                        "$29.99"
+                      )}
+                    </td>
+                  </tr>
+                  <tr className=" border-t border-solid border-semiGrey ">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-bold lg:text-[18px] text-[20px]">
+                      Order Total:
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-normal lg:text-[18px] text-[20px]">
+                      $
+                      {isShippingFree(total)
+                        ? total.toFixed(2)
+                        : orderTotal.toFixed(2)}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
       </div>
       <OrderSummary
         total={total}
