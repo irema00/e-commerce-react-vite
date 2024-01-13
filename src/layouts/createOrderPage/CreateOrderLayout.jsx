8b0946453786ba65975a835fdfccc3061f928dbc
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-14 01:39:07 +0300

    adds go back to cart icon

--- src/layouts/createOrderPage/CreateOrderLayout.jsx
+++ src/layouts/createOrderPage/CreateOrderLayout.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import OrderSummary from "../../components/OrderSummary";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "../../store/actions/shoppingCartActions";
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateOrderLayout() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { total, shippingCost, orderTotal } = useSelector(
     (state) => state.shoppingCart
   );
@@ -13,8 +16,24 @@ export default function CreateOrderLayout() {
     dispatch(calculateTotals());
   }, [dispatch]);
 
+  const handleGoCart = () => {
+    navigate("/cart");
+  };
   return (
-    <div className="container mx-auto my-6 p-4 bg-ltGrey rounded-xl flex justify-between gap-10">
+    <div className="container mx-auto my-6 p-4 bg-ltGrey rounded-xl flex justify-between gap-10 flex-col">
+      <div className="flex items-center gap-3 text-xs text-hdGrey font-semibold mt-3">
+        <Icon
+          icon="fluent-mdl2:back"
+          onClick={handleGoCart}
+          className="cursor-pointer text-gray-800"
+        />{" "}
+        <p
+          className="cursor-pointer hover:underline hover:text-semiGrey"
+          onClick={handleGoCart}
+        >
+          Go Back To My Cart
+        </p>
+      </div>
       <OrderSummary
         total={total}
         shippingCost={shippingCost}
