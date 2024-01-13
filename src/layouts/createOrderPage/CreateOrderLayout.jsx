b853c37067399286c4f1a0e83b0b487281acb819
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-14 02:22:24 +0300

    updates createOrderLayout draft by adding address information, payment options and delivery address boxes

--- src/layouts/createOrderPage/CreateOrderLayout.jsx
+++ src/layouts/createOrderPage/CreateOrderLayout.jsx
@@ -34,11 +34,70 @@ export default function CreateOrderLayout() {
           Go Back To My Cart
         </p>
       </div>
-      <OrderSummary
-        total={total}
-        shippingCost={shippingCost}
-        orderTotal={orderTotal}
-      />
+
+      <div className="flex w-full">
+        <div className="flex flex-col md:flex-row gap-4 p-4">
+          <div className="flex flex-col ">
+            <div className="flex gap-2 ">
+              <div className="mb-4 p-4 bg-white rounded shadow w-1/2">
+                <div className="flex justify-between gap-2">
+                  <h2 className="text-lg font-bold mb-3 ">
+                    Address Information
+                  </h2>
+                  <h1 className="text-2xl font-bold text-successGreen">1</h1>
+                </div>
+
+                <div className="flex flex-col items-start justify-between">
+                  <p className="flex text-prBlue font-bold ">Home</p>
+                  <span className="text-gray-600 text-sm">Ahmet Akyürek</span>
+                  <span className="text-gray-600 text-sm">
+                    Üniversiteler Mh.
+                  </span>
+                  <span className="text-gray-600 text-sm">
+                    06800- Ankara/Çankaya
+                  </span>
+                </div>
+              </div>
+              <div className="p-4 mb-4 bg-white rounded shadow w-1/2">
+                <div className="flex justify-between gap-2">
+                  <h2 className="text-lg font-bold mb-3 w-1/2">
+                    Payment Options
+                  </h2>
+                  <h1 className="text-2xl font-bold text-successGreen">2</h1>
+                </div>
+                <p className="text-sm text-gray-500">
+                  You can make your payment safely with a
+                  <span className="font-bold text-hdGrey">
+                    {" "}
+                    bank/credit card{" "}
+                  </span>
+                  or
+                  <span className="font-bold text-hdGrey">
+                    {" "}
+                    Shopping Credit
+                  </span>
+                  .
+                </p>
+              </div>
+            </div>
+            <div className="mt-4 p-4 bg-white rounded shadow">
+              <h2 className="text-lg font-bold mb-3">Delivery Address</h2>
+              <div className="flex items-center justify-between">
+                <p className="flex-1">Add New Address</p>
+                <button className="p-2 bg-gray-200 rounded">+</button>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <OrderSummary
+              total={total}
+              shippingCost={shippingCost}
+              orderTotal={orderTotal}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
